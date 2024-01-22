c06296707e3c2166b70a8c84e421ba1ddcfb0d2e
webp <114560928+studentifo@users.noreply.github.com>
2024-01-22 22:30:38 +0530

    main_controller.py

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -2,5 +2,4 @@ from flask import render_template
 
 class MainController:
     def index(self):
-        # Your logic to load and display notes goes here
         return render_template("index.html", notes=[])
