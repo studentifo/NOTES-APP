6b18bc3ce5fd7af6e9306d0c6a137045c300d4b6
webp <114560928+studentifo@users.noreply.github.com>
2024-01-22 22:31:00 +0530

    note_controller.py

--- app/controllers/note_controller.py
+++ app/controllers/note_controller.py
@@ -2,5 +2,4 @@ from flask import render_template
 
 class NoteController:
     def add_note(self):
-        # Your logic to add a note goes here
         return render_template("index.html", notes=[])
